3da93d6980401a2e849044b3544b5f21c9d98cd2
GustavoMendozaAren <gmendoza10@alumnos.uaq.mx>
2023-02-09 13:49:05 -0600

    PlayerScriptAnimator

--- TPAdventure/Assets/5.Scripts/2.PlayerScripts/PlayerScript.cs
+++ TPAdventure/Assets/5.Scripts/2.PlayerScripts/PlayerScript.cs
@@ -4,15 +4,93 @@ using UnityEngine;
 
 public class PlayerScript : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+
+    private Rigidbody MyBody;
+    private Animator Anim;
+
+    public Transform Cam;
+
+    private bool isPlayermoving;
+
+    private float playerSpeed = 0.03f;
+    private float rotationspeed = 4f;
+
+    private float jumpForce = 3f;
+    private bool canJump;
+
+    private float moveVertical;
+
+    private float rotY = 0;
+
+    public Transform groudCheck;
+    public LayerMask groundLayer;
+
+    private void Awake()
     {
-        
+        MyBody = GetComponent<Rigidbody>();
+        Anim = GetComponent<Animator>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Start()
     {
-        
+        rotY = transform.localRotation.eulerAngles.y;
     }
+
+    private void Update()
+    {
+        PlayerMoveKeyboard();
+        //Animations();
+    }
+
+    private void FixedUpdate()
+    {
+        MoveAndRotate();
+    }
+
+
+    void PlayerMoveKeyboard()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            moveVertical = 1;
+            Anim.SetTrigger(MyTags.WALK_TRIGGER);
+        }
+        else
+        {
+            moveVertical = 0;
+            Anim.SetTrigger(MyTags.STOP_TRIGGER);
+        }
+
+        /*if (Input.GetKeyUp(KeyCode.W))
+        {
+            moveVertical = 0;
+            Anim.SetTrigger(MyTags.STOP_TRIGGER);
+        }*/
+    }
+
+    void MoveAndRotate()
+    {
+        if(moveVertical != 0)
+        {
+            MyBody.MovePosition(transform.position + transform.forward * (moveVertical * playerSpeed));
+        }
+
+        MyBody.rotation = Quaternion.Euler(0f, Cam.eulerAngles.y, 0f);
+    }
+
+    /*void Animations()
+    {
+        if(moveVertical != 0)
+        {
+            if (!isPlayermoving)
+            {
+                if (!Anim.GetCurrentAnimatorStateInfo(0).IsName(MyTags.WALK_ANIMATION))
+                {
+                    isPlayermoving = true;
+                    Anim.SetTrigger(MyTags.WALK_TRIGGER);
+                }
+            }
+        }
+
+    }*/
 }
