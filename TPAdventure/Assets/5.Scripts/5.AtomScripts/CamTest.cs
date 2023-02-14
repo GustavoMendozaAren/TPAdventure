804e8712b5ccbb44c929edced6c1354ee13bcc31
GustavoMendozaAren <gmendoza10@alumnos.uaq.mx>
2023-02-13 22:17:58 -0600

    DiezYOcho

--- TPAdventure/Assets/5.Scripts/5.AtomScripts/CamTest.cs
+++ TPAdventure/Assets/5.Scripts/5.AtomScripts/CamTest.cs
@@ -5,41 +5,82 @@ using UnityEngine;
 public class CamTest : MonoBehaviour
 {
 
-    float velocidad = 0f, j = -1f, i = -20f;
+    float velocidad = 0f, Tiempo = 0f, DivAc = 20f, TiempoGen = 0f, Tiempo2 = 0f;
 
     void Update()
     {
+
+        TiempoGen += Time.deltaTime;
             
         transform.Rotate(new Vector3(0f, 0f, Time.deltaTime * 12.5f));
 
-        if (i < -2f)
+        velocidad -= (Time.deltaTime * 15f) / DivAc;
+
+        if (TiempoGen < 3f)
         {
-            i += Time.deltaTime * ;
+            Aceleracion();
         }
-        else
+        else if (TiempoGen >= 3f && TiempoGen < 27f)
         {
-            i = -2f;
+            transform.position = new Vector3(0f, 0f, -55f + (velocidad));
         }
-        if (velocidad >= -220f)
+        else if (TiempoGen >= 27f)
         {
-            velocidad -= Time.deltaTime * 11.25f;
+            Desaceleracion();
         }
-        if (velocidad < -220f)
+    }
+
+    private void Aceleracion()
+    {
+        Tiempo += Time.deltaTime;
+
+        if(Tiempo >= .2f)
         {
-            j = j - .01f;
-            velocidad += ((Time.deltaTime * 11.25f) / j);
-        }
-        if (j < -20f)
-        {
-            j = -20f;
-        }
-        if (velocidad < -280f)
-        {
-            velocidad = -280f;
+            Tiempo = 0f;
+
+            if (DivAc >= 8f)
+            {
+                DivAc -= 3f;
+            }
+            else if (DivAc < 8f && DivAc >= 2.6f)
+            {               
+                DivAc -= .6f;
+            }
+            else if (DivAc < 2.6f && DivAc >= 1.2f)
+            {
+                DivAc -= .2f;
+            }
+            else 
+            {
+                DivAc = 1f;
+            }
         }
 
         transform.position = new Vector3(0f, 0f, -55f + velocidad);
+    }
 
+    private void Desaceleracion()
+    {
+        Tiempo2 += Time.deltaTime;
 
+        if(Tiempo2 >= .2f)
+        {
+            Tiempo2 = 0f;
+
+            if(DivAc <= 1.8f)
+            {
+                DivAc += .2f;
+            }
+            else if (DivAc > 1.8f && DivAc <= 4.4f)
+            {
+                DivAc += .6f;
+            }
+            else if(DivAc > 4.4f && DivAc <= 20f)
+            {
+                DivAc += 3f;
+            }
+        }
+
+        transform.position = new Vector3(0f, 0f, -55f + velocidad);
     }
 }
