edfd892021e2d256029a93f9c39e36dc0612ab77
GustavoMendozaAren <gmendoza10@alumnos.uaq.mx>
2023-02-19 21:04:13 -0600

    PlayerBool

--- TPAdventure/Assets/5.Scripts/2.PlayerScripts/Player2Script.cs
+++ TPAdventure/Assets/5.Scripts/2.PlayerScripts/Player2Script.cs
@@ -16,41 +16,62 @@ public class Player2Script : MonoBehaviour
     float velocity = 0.0f;
     public float acceleration = 0.1f;
     public float deceleration = 0.1f;
+
+    int isWalkingHash, isRunningHash;
     int VelocityHash;
 
     private void Awake()
     {
         //MyBody = GetComponent<Rigidbody>();
         Anim = GetComponent<Animator>();
-        VelocityHash = Animator.StringToHash("Velocity");
-    }
 
-    void Start()
-    {
-        
+        isWalkingHash = Animator.StringToHash("isWalking");
+        isRunningHash = Animator.StringToHash("isRunning");
+        VelocityHash = Animator.StringToHash("Velocity");
     }
 
     void Update()
     {
-        PlayerMoveKeyboard();
+        //PlayerMovementBool();
+        PlayerMovementBlendTree();
     }
 
-    private void FixedUpdate()
+    /*void PlayerMovementBool()
     {
-        //MoveAndRotate();
-    }
+        bool isRunning = Anim.GetBool(isRunningHash);
+        bool isWalking = Anim.GetBool(isWalkingHash);
+        bool forwardPress = Input.GetKey(KeyCode.W);
+        bool runPressed = Input.GetKey(KeyCode.LeftShift);
 
-    void PlayerMoveKeyboard()
+        if (!isWalking && forwardPress)
+        {
+            Anim.SetBool(isWalkingHash, true);
+        }
+        if (isWalking && !forwardPress)
+        {
+            Anim.SetBool(isWalkingHash, false);
+        }
+        if(!isRunning && (forwardPress && runPressed))
+        {
+            Anim.SetBool(isRunningHash, true);
+        }
+        if(isRunning && (!forwardPress || !runPressed))
+        {
+            Anim.SetBool(isRunningHash, false);
+        }
+    }*/
+
+    void PlayerMovementBlendTree()
     {
         bool forwardPressed = Input.GetKey(KeyCode.W);
         bool runPressed = Input.GetKey(KeyCode.LeftShift);
 
-        if (forwardPressed && velocity < 1.0f)
+        if (forwardPressed)
         {
             velocity += Time.deltaTime * acceleration;
         }
 
-        if (!forwardPressed && velocity > 0.0f)
+        /*if (!forwardPressed && velocity > 0.0f)
         {
             velocity -= Time.deltaTime * deceleration;
         }
@@ -58,7 +79,7 @@ public class Player2Script : MonoBehaviour
         if (!forwardPressed && velocity < 0.0f)
         {
             velocity = 0.0f;
-        }
+        }*/
 
         Anim.SetFloat(VelocityHash, velocity);
     }
